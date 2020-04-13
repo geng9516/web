379f489c0d3578271201ca412a61a8d22c25b500
肖 文彭 <X02413@tokyo.scientia.co.jp>
2020-04-13 15:25:01 +0900

    feats: 添加模板公共文件

--- src/main/java/jp/smartcompany/job/configuration/WebConfiguration.java
+++ src/main/java/jp/smartcompany/job/configuration/WebConfiguration.java
@@ -4,17 +4,15 @@ import jp.smartcompany.job.interceptor.AuditInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.*;
 
 /**
  * @author Xiao Wenpeng
  */
 @Configuration
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
-public class WebConfiguration implements WebMvcConfigurer {
+public class WebConfiguration extends WebMvcConfigurationSupport {
 
     private final AuditInterceptor auditInterceptor;
 
@@ -37,6 +35,9 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");
+        super.addResourceHandlers(registry);
 //        registry.addResourceHandler("/upload/**")
 //                .addResourceLocations("file:///D:/IdeaWorkspace/office-next-BE/upload/");
     }
