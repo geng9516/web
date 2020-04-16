4d30ada94ef93a7aeb07197306004e9787cc3583
肖 文彭 <X02413@tokyo.scientia.co.jp>
2020-04-16 11:26:35 +0900

    style: 去除示例代码

--- src/main/java/jp/smartcompany/job/modules/base/service/PluggableService.java
+++ src/main/java/jp/smartcompany/job/modules/base/service/PluggableService.java
@@ -1,15 +1,9 @@
 package jp.smartcompany.job.modules.base.service;
 
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.StrUtil;
-import cn.hutool.extra.spring.SpringUtil;
 import jp.smartcompany.job.common.GlobalException;
 import jp.smartcompany.job.modules.base.BaseBean;
 import jp.smartcompany.job.modules.base.pojo.bo.PluggableBO;
-import jp.smartcompany.job.modules.tmg.vacation.VacationBean;
-import jp.smartcompany.job.modules.tmg.vacation.service.VacationService;
-import jp.smartcompany.job.modules.tmg.work.WorkBean;
-import jp.smartcompany.job.modules.tmg.work.service.WorkService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,8 +17,8 @@ public class PluggableService {
 
     public String execute() {
         List<String> classNames = CollUtil.newArrayList();
-        classNames.add(WorkBean.Service.work);
-        classNames.add(VacationBean.Service.vacation);
+//        classNames.add(WorkBean.Service.work);
+//        classNames.add(VacationBean.Service.vacation);
         // 初始化参数
         PluggableBO pluggableBO = new PluggableBO();
         pluggableBO.setParam1("p1");
@@ -39,19 +33,19 @@ public class PluggableService {
 
     @Transactional(rollbackFor = GlobalException.class)
     public String executeTask(String className,PluggableBO pluggableBO) {
-        String errorMessage;
-        // 执行勤务service里的方法
-        if (StrUtil.equals(WorkBean.Service.work,className)) {
-            WorkService workService = SpringUtil.getBean(className);
-            errorMessage = workService.execute(pluggableBO);
-        // 执行休假service里的方法
-        } else if (StrUtil.equals(VacationBean.Service.vacation,className)) {
-            VacationService vacationService = SpringUtil.getBean(className);
-            errorMessage = vacationService.execute(pluggableBO);
-        // 执行没有报错的情况没有错误信息
-        } else {
-           errorMessage = null;
-        }
+        String errorMessage=null;
+//        // 执行勤务service里的方法
+//        if (StrUtil.equals(WorkBean.Service.work,className)) {
+//            WorkService workService = SpringUtil.getBean(className);
+//            errorMessage = workService.execute(pluggableBO);
+//        // 执行休假service里的方法
+//        } else if (StrUtil.equals(VacationBean.Service.vacation,className)) {
+//            VacationService vacationService = SpringUtil.getBean(className);
+//            errorMessage = vacationService.execute(pluggableBO);
+//        // 执行没有报错的情况没有错误信息
+//        } else {
+//           errorMessage = null;
+//        }
         return errorMessage;
     }
 
