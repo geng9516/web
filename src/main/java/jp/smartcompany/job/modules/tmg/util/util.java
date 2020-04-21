c4d5840110ab018ac56d286a644e9a0d3b012180
wzy <wzy19950420@gmail.com>
2020-04-21 17:27:20 +0900

    更新

--- src/main/java/jp/smartcompany/job/modules/tmg/util/util.java
+++ src/main/java/jp/smartcompany/job/modules/tmg/util/util.java
@@ -3,14 +3,18 @@ package jp.smartcompany.job.modules.tmg.util;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.StrUtil;
+import jp.smartcompany.job.modules.core.pojo.entity.MastOrganisationDO;
 import jp.smartcompany.job.modules.core.service.IConfSyscontrolService;
+import jp.smartcompany.job.modules.core.service.IMastOrganisationService;
 import jp.smartcompany.job.modules.core.service.ITmgPaidHolidayAttributeService;
 import jp.smartcompany.job.modules.core.service.ITmgPersonalPatternService;
-import jp.smartcompany.job.modules.tmg.PatternSetting.TmgFGetPatternDefault;
+import jp.smartcompany.job.modules.tmg.paidholiday.TmgConvFraction4nyk;
+import jp.smartcompany.job.modules.tmg.patternsetting.TmgFGetPatternDefault;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -22,9 +26,9 @@ public class util {
     private final ITmgPaidHolidayAttributeService iTmgPaidHolidayAttributeService;
     private final ITmgPersonalPatternService iTmgPersonalPatternService;
     private final IConfSyscontrolService iConfSyscontrolService;
-    private TmgFGetPatternDefault tmgFGetPatternDefault;
-
-
+    private final TmgFGetPatternDefault tmgFGetPatternDefault;
+    private final TmgConvFraction4nyk tmgConvFraction4nyk;
+    private final IMastOrganisationService iMastOrganisationService;
     /**
      *平均勤務時間の換算処理
      * @param customerId 法人コード
@@ -40,7 +44,7 @@ public class util {
         //平均勤務時間を取得する
         returnValue=tmgFgGetAvgWorkTime(customerId,companyId,employeeId,yyyymmdd);
         //換算処理
-        //returnValue=tmgConvFraction4Nyk(customerId,companyId,employeeId,yyyymmdd,returnValue);
+        returnValue=tmgConvFraction4nyk.init(customerId,companyId,employeeId,yyyymmdd,returnValue);
 
         return returnValue;
 }
@@ -100,6 +104,47 @@ public class util {
         }else{
             return propertyValue;
         }
+
+    }
+
+
+    /**
+     *上位組織を取得
+     * @param customerId 法人コード
+     * @param companyId 顧客コード
+     * @param sectionId 組織番号
+     * @param yyyymmdd 基準日
+     * @return List<String>  上位組織
+     * */
+    public List<String>  tmgGetUpperSection (String customerId, String companyId, String sectionId, Date yyyymmdd){
+        Date baseDate=(Date)sqlUtil.nvlSql(yyyymmdd,DateUtil.date());
+        List<String> sectionIdList=new ArrayList<String>();
+        //取得フラグ
+        boolean loopFlg=false;
+        String upperSectionId="";
+        if(StrUtil.hasEmpty(sectionId)){
+            List<MastOrganisationDO> moDoList=iMastOrganisationService.selectPatternOrganisation(customerId,companyId,baseDate);
+            for(MastOrganisationDO mODo:moDoList){
+                sectionIdList.add(mODo.getMoCsectionidCk());
+            }
+        }else{
+            sectionIdList.add(sectionId);
+            upperSectionId=sectionId;
+            //上位組織を取得
+            do{
+                loopFlg=false;
+                MastOrganisationDO moDo=iMastOrganisationService.selectOrganisation(customerId,companyId,upperSectionId,baseDate);
+                if(moDo!=null&&!StrUtil.hasEmpty(moDo.getMoCsectionidCk())){
+                    //上位組織を取得
+                    upperSectionId= moDo.getMoCsectionidCk();
+                    //上位組織が取得出来た場合は対象にセット
+                    sectionIdList.add(upperSectionId);
+                    loopFlg=true;
+                }
+
+            }while (loopFlg);
+        }
+        return sectionIdList;
 
     }
 }
