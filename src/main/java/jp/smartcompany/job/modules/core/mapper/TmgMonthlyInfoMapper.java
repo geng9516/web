6d5e41dc7ad93813b21bde831d4d2f08a2e3aaa4
nie.wanqun@nissin-sci.co.jp <password>
2020-06-02 16:32:55 +0900

    就業承認一覧追加

--- src/main/java/jp/smartcompany/job/modules/core/mapper/TmgMonthlyInfoMapper.java
+++ src/main/java/jp/smartcompany/job/modules/core/mapper/TmgMonthlyInfoMapper.java
@@ -2,7 +2,11 @@ package jp.smartcompany.job.modules.core.mapper;
 
 import jp.smartcompany.job.modules.core.pojo.entity.TmgMonthlyInfoDO;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import jp.smartcompany.job.modules.tmg.permStatList.vo.TmgMonthlyInfoVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,4 +20,12 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface TmgMonthlyInfoMapper extends BaseMapper<TmgMonthlyInfoDO> {
 
-        }
+    /**
+     * 勤怠データ件数
+     */
+    int buildSQLForSelectTmgMonthlyInfoCount(Map<String, Object> map);
+    /**
+     * 表示する職員氏名と、承認ステータス状態を取得する
+     */
+    List<TmgMonthlyInfoVO> buildSQLForSelectTmgMonthlyInfo(Map<String, Object> map);
+}
--- src/main/java/jp/smartcompany/job/modules/core/service/ITmgMonthlyInfoService.java
+++ src/main/java/jp/smartcompany/job/modules/core/service/ITmgMonthlyInfoService.java
@@ -2,6 +2,10 @@ package jp.smartcompany.job.modules.core.service;
 
 import jp.smartcompany.job.modules.core.pojo.entity.TmgMonthlyInfoDO;
 import com.baomidou.mybatisplus.extension.service.IService;
+import jp.smartcompany.job.modules.tmg.permStatList.dto.ColNameDto;
+import jp.smartcompany.job.modules.tmg.permStatList.vo.TmgMonthlyInfoVO;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +17,26 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface ITmgMonthlyInfoService extends IService<TmgMonthlyInfoDO> {
 
-        }
+    /**
+     * 勤怠データ件数
+     *
+     * @param empSql 対象者取得SQL
+     * @param date   　対象月
+     * @return
+     */
+    int buildSQLForSelectTmgMonthlyInfoCount(String empSql, String date);
+
+    /**
+     * 表示する職員氏名と、承認ステータス状態を取得する
+     *
+     * @param custId   顧客コード
+     * @param compId   法人コード
+     * @param baseDate 　対象年月
+     * @param lang     　言語
+     * @param today    　今日
+     * @param empSql   　対象者取得ｓql
+     * @param list     　承認ステータス状態項目
+     */
+    List<TmgMonthlyInfoVO> buildSQLForSelectTmgMonthlyInfo(String custId, String compId, String baseDate, String lang, String today, String empSql, List<ColNameDto> list);
+
+}
