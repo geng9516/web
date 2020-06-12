0164544669e0f5b163c22c833548c6c32f9b41fa
CHENYILI <chen.yili@nisshin-sci.co.jp>
2020-06-12 15:40:51 +0900

    author by chenyili 勤務パターン設定

---
+++ src/main/java/jp/smartcompany/job/modules/tmg/PatternSetting/PatternSettingBean.java
@@ -0,0 +1,14 @@
+package jp.smartcompany.job.modules.tmg.PatternSetting;
+
+/**
+ * @author 陳毅力
+ * @description 勤務パターン
+ * @objectSource ps.c01.tmg.PatternSetting.PatternSettingBean
+ * @date 2020/06/12
+ **/
+public class PatternSettingBean {
+
+
+
+
+}
