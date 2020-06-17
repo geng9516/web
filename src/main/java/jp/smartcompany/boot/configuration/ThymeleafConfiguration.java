7ea8f6383d8fd2ea57bf86f19418fbea45dde476
肖文彭 <ja_xwp@yeah.net>
2020-06-17 18:40:11 +0900

    feats: 加上thymeleaf的热加载功能

---
+++ src/main/java/jp/smartcompany/boot/configuration/ThymeleafConfiguration.java
@@ -0,0 +1,25 @@
+package jp.smartcompany.boot.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+/**
+ * @author Xiao Wenpeng
+ */
+@Configuration
+public class ThymeleafConfiguration {
+
+    @Bean
+    public ITemplateResolver defaultTemplateResolver() {
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setSuffix(".html");
+        resolver.setPrefix("src/main/resources/templates/");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(false);
+        return resolver;
+    }
+
+}
