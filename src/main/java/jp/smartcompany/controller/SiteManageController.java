ede57afede9d086e889f4d68185b2b28b68a7582
CHENYILI <chen.yili@nisshin-sci.co.jp>
2020-06-23 13:21:50 +0900

    author by chenyili 予定作成コントロールを追加しました

--- src/main/java/jp/smartcompany/controller/SiteManageController.java
+++ src/main/java/jp/smartcompany/controller/SiteManageController.java
@@ -88,6 +88,29 @@ public class SiteManageController {
         return "sys/manage/attendancebook";
     }
 
+    @GetMapping("toTmgSchedule")
+    public String toTmgSchedule(
+            @RequestAttribute("BeanName") PsDBBean psDBBean,
+            @RequestParam("moduleIndex") Integer moduleIndex,
+            @RequestParam("menuId") Long menuId, ModelMap modelMap) throws Exception {
+        String baseDate = DateUtil.format(DateUtil.date(), TmgReferList.DEFAULT_DATE_FORMAT);
+        TmgReferList referList = new TmgReferList(psDBBean, psDBBean.getAppId(), baseDate, TmgReferList.TREEVIEW_TYPE_EMP, true,
+                true, false, false, true);
+        modelMap
+                .addAttribute("moduleIndex",moduleIndex)
+                .addAttribute("menuId",menuId)
+                .addAttribute("targetSection",referList.getTargetSec())
+                .addAttribute("targetGroup",referList.getTargetGroup())
+                .addAttribute(TmgReferList.ATTR_TREEVIEW_RECORD_DATE,TmgReferList.TREEVIEW_KEY_RECORD_DATE)
+                .addAttribute(TmgReferList.ATTR_TREEVIEW_REFRESH_FLG,TmgReferList.TREEVIEW_KEY_REFRESH_FLG)
+                .addAttribute(TmgReferList.ATTR_TREEVIEW_PERM_TARGET_EMP,TmgReferList.TREEVIEW_KEY_PERM_TARGET_EMP)
+                .addAttribute(TmgReferList.ATTR_TREEVIEW_PERM_TARGET_SECTION,TmgReferList.TREEVIEW_KEY_PERM_TARGET_SECTION)
+                .addAttribute(TmgReferList.ATTR_TREEVIEW_PERM_TARGET_GROUP,TmgReferList.TREEVIEW_KEY_PERM_TARGET_GROUP);
+        return "sys/manage/tmgschedule";
+    }
+
+
+
     /**
      * 跳转到承認状況一覧界面
      * @param moduleIndex
