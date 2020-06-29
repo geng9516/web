2263b9a903bf072250ad655485b9761abd0eae97
dellenshang <dellenshang@gmail.com>
2020-06-29 19:26:57 +0900

    fix: 方法名修正

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -163,7 +163,7 @@ const Utils = {
   findChildrens(context, componentName) {
     return context.$children.reduce((components, child) => {
       if (child.$options.name === componentName) components.push(child)
-      const foundChilds = allChildrens(child, componentName)
+      const foundChilds = findChildrens(child, componentName)
       return components.concat(foundChilds)
     }, [])
   }
