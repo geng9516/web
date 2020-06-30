e6b089798c7ab3ae8d6388ad902cc0dbd3390c61
肖文彭 <ja_xwp@yeah.net>
2020-06-30 17:17:21 +0900

    fix: row_number拼写错误

--- src/main/java/jp/smartcompany/boot/interceptor/SqlParameter2NullInterceptor.java
+++ src/main/java/jp/smartcompany/boot/interceptor/SqlParameter2NullInterceptor.java
@@ -44,8 +44,8 @@ public class SqlParameter2NullInterceptor implements Interceptor {
             originalSql=originalSql.replaceAll("(\\(\\s*,)","(null,");
             originalSql=originalSql.replaceAll("(,\\s*\\))",",null)");
             // 防止系统函数也被填充null值
-            originalSql=originalSql.replaceAll("ROW_NUMBER\\s*\\(\\s*NULL\\s*\\)","ROWNUM()");
-            originalSql=originalSql.replaceAll("ROW_NUMBER\\s*\\(\\s*null\\s*\\)","ROWNUM()");
+            originalSql=originalSql.replaceAll("ROW_NUMBER\\s*\\(\\s*NULL\\s*\\)","ROW_NUMBER()");
+            originalSql=originalSql.replaceAll("ROW_NUMBER\\s*\\(\\s*null\\s*\\)","ROW_NUMBER()");
             metaObject.setValue("delegate.boundSql.sql", originalSql);
         }
         return invocation.proceed();
