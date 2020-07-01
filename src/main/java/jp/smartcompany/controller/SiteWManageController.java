2c295a151484a8d0e5205e164b2eee24d1b6c3ab
dellen shang <delenshang@gmail.com>
2020-07-01 10:59:52 +0900

    feat: 休假承认路由

--- src/main/java/jp/smartcompany/controller/SiteWManageController.java
+++ src/main/java/jp/smartcompany/controller/SiteWManageController.java
@@ -223,4 +223,18 @@ public class SiteWManageController {
         return "sys/wmanage/wsum";
     }
 
+    /**
+     * 跳转到休暇承認界面
+     * @param moduleIndex
+     * @param menuId
+     * @param modelMap
+     * @return
+     */
+    @GetMapping("tmgnotification")
+    public String toTmgNotification(@RequestParam("moduleIndex") Integer moduleIndex,
+                               @RequestParam("menuId") Long menuId, ModelMap modelMap) {
+        modelMap.addAttribute("moduleIndex",moduleIndex)
+                .addAttribute("menuId",menuId);
+        return "sys/wmanage/tmgnotification";
+    }
 }
