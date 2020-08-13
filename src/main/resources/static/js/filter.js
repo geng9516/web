8fa275ab5cca061d337476b4b8ed31bf709b4b7f
dellen shang <delenshang@gmail.com>
2020-08-13 18:22:42 +0900

    fix: 格式化时间显示不加0

--- src/main/resources/static/js/filter.js
+++ src/main/resources/static/js/filter.js
@@ -5,19 +5,19 @@ Vue.filter('handleTime', function (e) {
   }
   const E = Math.abs(e)
   if (E >= 6000) {
-    const result = `0${(E / 60) ^ 0}`.slice(-3) + ':' + ('0' + (E % 60)).slice(-2)
+    const result = `${(E / 60) ^ 0}`.slice(-3) + ':' + ('0' + (E % 60)).slice(-2)
     return e < 0 ? `-${result}` : result
   }
-  const result = `0${(E / 60) ^ 0}`.slice(-2) + ':' + ('0' + (E % 60)).slice(-2)
+  const result = `${(E / 60) ^ 0}`.slice(-2) + ':' + ('0' + (E % 60)).slice(-2)
   return e < 0 ? `-${result}` : result
 })
 
 // 给天数和时间数字增加精度
 Vue.filter('addNumDeci', function (e, isDay) {
   if (e === '-'){
-    return e
+    return '-'
   }
-  if (!e　) {
+  if (!e) {
     if (!isDay) return '0.00'
       return '0.0'
   }
