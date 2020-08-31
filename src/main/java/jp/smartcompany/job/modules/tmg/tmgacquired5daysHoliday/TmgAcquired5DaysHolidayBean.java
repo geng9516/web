17f2b76316821144782c3f9f8906491e2ee628e8
gu.chengbin@nisshin-sci.co.jp <gcb87709036>
2020-08-31 17:27:30 +0900

    fix：年5修正

--- src/main/java/jp/smartcompany/job/modules/tmg/tmgacquired5daysHoliday/TmgAcquired5DaysHolidayBean.java
+++ src/main/java/jp/smartcompany/job/modules/tmg/tmgacquired5daysHoliday/TmgAcquired5DaysHolidayBean.java
@@ -168,10 +168,10 @@ public class TmgAcquired5DaysHolidayBean {
             tmgPaiduseinfoFixDO.setTpfFuyobi(DateUtil.parse(updateAcquired5DaysVO.getTxtFuyobi()));
             tmgPaiduseinfoFixDO.setTpfDpaidHolidayFix(DateUtil.parse(updateAcquired5DaysVO.getKijunbiEdit()));
             tmgPaiduseinfoFixDO.setTpfDkikanbiFix(DateUtil.parse(updateAcquired5DaysVO.getDkikanbiFix()));
-            if ("".equals(updateAcquired5DaysVO.getUsedDaysEdit()) || updateAcquired5DaysVO.getUsedDaysEdit() != null) {
+            if (!"".equals(updateAcquired5DaysVO.getUsedDaysEdit()) && updateAcquired5DaysVO.getUsedDaysEdit() != null) {
                 tmgPaiduseinfoFixDO.setTpfNusedaysAjdust(Double.parseDouble(updateAcquired5DaysVO.getUsedDaysEdit()));
             }
-            if ("".equals(updateAcquired5DaysVO.getTxtNusedaysDays()) || updateAcquired5DaysVO.getTxtNusedaysDays() != null) {
+            if (!"".equals(updateAcquired5DaysVO.getTxtNusedaysDays()) && updateAcquired5DaysVO.getTxtNusedaysDays() != null) {
                 tmgPaiduseinfoFixDO.setTpfNmustdaysFix(Double.parseDouble(updateAcquired5DaysVO.getTxtNusedaysDays()));
             }
             iTmgPaiduseinfoFixService.getBaseMapper().insert(tmgPaiduseinfoFixDO);
