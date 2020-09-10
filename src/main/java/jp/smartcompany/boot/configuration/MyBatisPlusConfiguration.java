b7ac310414a6245ed8ffa829b0bfecca3e9d0b8b
肖文彭 <ja_xwp@yeah.net>
2020-09-10 10:48:39 +0900

    feats: 去除多余import，改为社内oracle连接

--- src/main/java/jp/smartcompany/boot/configuration/MyBatisPlusConfiguration.java
+++ src/main/java/jp/smartcompany/boot/configuration/MyBatisPlusConfiguration.java
@@ -10,8 +10,6 @@ import jp.smartcompany.boot.interceptor.SqlParameter2NullInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 /**
  * @author Xiao　Wenpeng
  *　MyBatisPlus核心
