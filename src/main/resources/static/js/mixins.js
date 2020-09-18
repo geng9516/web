b37456178ab48e3f06162f2e177784b4a7071936
dellen shang <delenshang@gmail.com>
2020-09-18 11:00:47 +0900

    fix: 加入1970/01/01 08:00 限制

---
+++ src/main/resources/static/js/mixins.js
@@ -0,0 +1,12 @@
+// 全局混入的限制1970/01/01 08:00
+Vue.mixin({
+    data () {
+      return {
+        startLimit: {
+          disabledDate: date => {
+          return date.valueOf() < -3600000
+          }
+        }
+      }
+    }
+})
