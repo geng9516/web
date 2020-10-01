9e4c36ae0da648d4b81bb4636643653fcdadc81f
肖文彭 <ja_xwp@yeah.net>
2020-10-01 12:01:34 +0900

    feats: 增加条件接口

--- src/main/java/jp/smartcompany/admin/searchrangemanager/logic/SearchRangeManagerLogic.java
+++ src/main/java/jp/smartcompany/admin/searchrangemanager/logic/SearchRangeManagerLogic.java
@@ -1,11 +1,17 @@
 package jp.smartcompany.admin.searchrangemanager.logic;
 
+import jp.smartcompany.job.modules.core.pojo.entity.MastDatapermissionDO;
+
 import java.util.Date;
 import java.util.Map;
+import java.util.List;
 
 public interface SearchRangeManagerLogic {
 
    Map<String,Object> listRangeTable(String systemId, Date searchDate, String groupId,
                                      String siteId, String appId,
                                      String language, String custId, String companyId, boolean isAll);
+
+   List<MastDatapermissionDO> listConditions();
+
 }
