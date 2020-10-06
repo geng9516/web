578eeed4a6339270966824c7ec49af032070f3b7
肖文彭 <ja_xwp@yeah.net>
2020-10-06 10:34:46 +0900

    fix: 修复搜索关键字保存的问题

--- src/main/java/jp/smartcompany/job/modules/tmg/util/TmgReferList.java
+++ src/main/java/jp/smartcompany/job/modules/tmg/util/TmgReferList.java
@@ -662,10 +662,12 @@ public class TmgReferList {
      */
     private void sessionControl4SearchTree(int piParam, List pvSearchDataArray, String psDispLimit4Tree){
         HttpSession httpSession = ContextUtil.getSession();
-        httpSession.setAttribute(SESSION_KEY_SEARCHDATAARRAY, pvSearchDataArray);
-        httpSession.setAttribute(SESSION_KEY_SEARCHCONDITION, String.valueOf(getSearchCondition()));
-        httpSession.setAttribute(SESSION_KEY_SEARCHDATA, String.valueOf(getSearchData()));
-        httpSession.setAttribute(SESSION_KEY_DISPLIMIT4TREE, String.valueOf(psDispLimit4Tree));
+        if (isSelectedSearchTab()) {
+            httpSession.setAttribute(SESSION_KEY_SEARCHDATAARRAY, pvSearchDataArray);
+            httpSession.setAttribute(SESSION_KEY_SEARCHCONDITION, String.valueOf(getSearchCondition()));
+            httpSession.setAttribute(SESSION_KEY_SEARCHDATA, String.valueOf(getSearchData()));
+            httpSession.setAttribute(SESSION_KEY_DISPLIMIT4TREE, String.valueOf(psDispLimit4Tree));
+        }
 //        switch(piParam){
 //            case csSessionControl4SearchTreeInitialization:
 //
