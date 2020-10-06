39cbd1980d3cb753a5d6b24fb1dffcc362bf287f
肖文彭 <ja_xwp@yeah.net>
2020-10-06 16:00:52 +0900

    fix: 加入搜索参数非空判断，防止被空条件覆盖

--- src/main/java/jp/smartcompany/job/modules/tmg/util/TmgReferList.java
+++ src/main/java/jp/smartcompany/job/modules/tmg/util/TmgReferList.java
@@ -663,7 +663,8 @@ public class TmgReferList {
     private void sessionControl4SearchTree(int piParam, List pvSearchDataArray, String psDispLimit4Tree){
         HttpSession httpSession = ContextUtil.getSession();
         httpSession.setAttribute(TREEVIEW_OBJ_HIDSELECT, getHidSelectTab());
-        if (isSelectedSearchTab()) {
+
+        if (isSelectedSearchTab() && StrUtil.isNotBlank(getSearchData())) {
 //            httpSession.setAttribute(SESSION_KEY_SEARCHDATAARRAY, pvSearchDataArray);
             httpSession.setAttribute(TREEVIEW_OBJ_HIDSEARCHCONDITION , String.valueOf(getSearchCondition()));
             httpSession.setAttribute(TREEVIEW_OBJ_HIDSEARCHITEMES, String.valueOf(getSearchItems()));
