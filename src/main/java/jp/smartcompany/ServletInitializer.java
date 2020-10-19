2af817415f64a166b8c84340eef93591263f47da
肖文彭 <ja_xwp@yeah.net>
2020-10-19 13:11:33 +0900

    fix: 修复删除group后员工消失的问题

--- src/main/java/jp/smartcompany/ServletInitializer.java
+++ src/main/java/jp/smartcompany/ServletInitializer.java
@@ -3,19 +3,11 @@ package jp.smartcompany;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(NextJobApplication.class);
-    }
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        super.onStartup(servletContext);
     }
 
 }
