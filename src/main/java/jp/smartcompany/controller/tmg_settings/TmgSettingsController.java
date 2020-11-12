c23d45d35bf6df13136a12a6dad33e4c167d9033
肖文彭 <ja_xwp@yeah.net>
2020-11-12 17:29:31 +0900

    feats: 添加系统设定site

---
+++ src/main/java/jp/smartcompany/controller/tmg_settings/TmgSettingsController.java
@@ -0,0 +1,22 @@
+package jp.smartcompany.controller.tmg_settings;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("tmg_settings")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class TmgSettingsController {
+
+    @GetMapping("TmgMasterSettings")
+    public String toTmgMasterSettings(@RequestParam("moduleIndex") Integer moduleIndex, ModelMap modelMap) {
+        modelMap.addAttribute("moduleIndex",moduleIndex);
+        return "sys/master_settings/namesettings";
+    }
+
+}
