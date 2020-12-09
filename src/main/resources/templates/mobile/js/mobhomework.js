a5149eb92c4d1d39f0b2f6d1871a2645d4fd774d
gu.chengbin@nisshin-sci.co.jp <gcb87709036>
2020-12-09 09:00:54 +0900

    fix: 在宅勤务 mobなし修正

--- src/main/resources/templates/mobile/js/mobhomework.js
+++ src/main/resources/templates/mobile/js/mobhomework.js
@@ -6,11 +6,19 @@ new Vue({
       hwhomeworkShow: false,
       StatusList0: [
         {
+          value: '0',
+          text: 'なし',
+        },
+        {
           value: '1',
           text: '承認待',
         }
       ],
       StatusList: [
+        {
+          value: '0',
+          text: 'なし',
+        },
         {
           value: '1',
           text: '承認待',
