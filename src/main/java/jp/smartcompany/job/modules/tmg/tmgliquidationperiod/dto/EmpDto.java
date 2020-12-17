fc737e85daf183f4c7855dd8f2b13dfe7b0189c3
wzy <wzy19950420@gmail.com>
2020-12-17 17:07:38 +0900

    承认site 显示权限

---
+++ src/main/java/jp/smartcompany/job/modules/tmg/tmgliquidationperiod/dto/EmpDto.java
@@ -0,0 +1,33 @@
+package jp.smartcompany.job.modules.tmg.tmgliquidationperiod.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class EmpDto {
+    private String level;
+    private String label;
+    private String grouplabel;
+    private String groupid;
+    private String groupname;
+    private String empid;
+    private String empname;
+    private String secid;
+    private String secnic;
+    private String postid;
+    private String postname;
+    private String workertypeid;
+    private String wokertypename;
+    private String dstart;
+    private String dend;
+    private String seq;
+    private String cust;
+    private String comp;
+    private String kananame;
+
+
+
+}
---
+++ src/main/java/jp/smartcompany/job/modules/tmg/tmgliquidationperiod/dto/JsonDto.java
@@ -0,0 +1,13 @@
+package jp.smartcompany.job.modules.tmg.tmgliquidationperiod.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class JsonDto {
+    private  EmpDto data;
+    private  boolean open;
+}
