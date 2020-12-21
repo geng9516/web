67f7b4307a0f48cdcecfd1828b4b094634f7f82d
肖文彭 <ja_xwp@yeah.net>
2020-12-21 14:25:14 +0900

    feats: 预加载系统常量到缓存中

--- src/main/java/jp/smartcompany/NextJobApplication.java
+++ src/main/java/jp/smartcompany/NextJobApplication.java
@@ -1,7 +1,11 @@
 package jp.smartcompany;
 
+import jp.smartcompany.boot.util.SpringUtil;
+import jp.smartcompany.job.modules.core.service.IMastSystemService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 
 /**
  * 系统启动类
@@ -11,7 +15,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NextJobApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NextJobApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(NextJobApplication.class);
+        // 预加载系统公共配置到缓存中
+        springApplication.addListeners((ApplicationListener<ApplicationReadyEvent>) event -> {
+            IMastSystemService systemService = SpringUtil.getBean(IMastSystemService.class);
+            systemService.getTableInfo();
+            systemService.selectSearchRangeInfo();
+            systemService.getByLang("ja");
+        });
+        springApplication.run(args);
     }
 
 }
